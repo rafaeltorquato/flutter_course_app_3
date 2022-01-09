ec788a3b4b9dd615830a0e174a63dae861c120e1
Rafael Torquato <rafaeltorquat0@protonmail.com>
2022-01-08 21:37:09 -0300

    ✨ Added bottom tabs navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,48 +3,51 @@ import 'package:flutter/material.dart';
 import '../widgets/favorites_list.dart';
 import '../widgets/categories_grid.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _widgets = const [
+    CaregoriesGrid(),
+    FavoritesList(),
+  ];
+  final _widgetsLabel = const [
+    'Categories',
+    'Favorites',
+  ];
+  int _selectedIndex = 0;
+
+  void _handleTap(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Meals'),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.category),
-                      SizedBox(width: 5),
-                      Text(
-                        'Categories',
-                      ),
-                    ],
-                  ),
-                ),
-                Tab(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.star),
-                      SizedBox(width: 5),
-                      Text('Favorites'),
-                    ],
-                  ),
-                )
-              ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_widgetsLabel[_selectedIndex]),
+        ),
+        body: _widgets[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categories',
             ),
-          ),
-          body: const TabBarView(children: [
-            CaregoriesGrid(),
-            FavoritesList(),
-          ]),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favorites',
+            )
+          ],
+          onTap: _handleTap,
+          currentIndex: _selectedIndex,
         ),
       ),
     );
